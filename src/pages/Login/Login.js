3c3c017c244b8092a9e83332fb9d706e8544f3a1
mdmahfujarrahmane <ahmedmahfuj28@gmail.com>
2022-10-23 11:51:57 +0600

    log in page added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,53 @@
-import "./Login.scss"
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import { useState } from "react";
+import "./Login.scss";
 
 const Login = () => {
+    const [show, setShow] = useState(false)
+
+
+
+
   return (
-    <div>Login</div>
-  )
+      <div class="login">
+          <div className="card">
+              <div className="left">
+                  <h1>Hello World!</h1>
+                  <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aspernatur, sequi alias placeat consectetur vel nam nemo
+                      error, laudantium voluptas odit velit adipisci eveniet et,
+                      in dignissimos dolores harum iste quas?
+                  </p>
+                  <span>Don't you have a account</span>
+                  <button>Register</button>
+              </div>
+              <div className="right">
+                  <h2>Login</h2>
+                  <form>
+                      <input type="text" placeholder="Username" />
+                      <input
+                          type={show ? "text" : "password"}
+                          placeholder="Password"
+                      />
+                      <div className="eyeIcon">
+                          {show ? (
+                              <VisibilityOutlinedIcon
+                                  onClick={() => setShow(!show)}
+                              />
+                          ) : (
+                              <VisibilityOffOutlinedIcon
+                                  onClick={() => setShow(!show)}
+                              />
+                          )}
+                      </div>
+                      <button>Login</button>
+                  </form>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default Login
